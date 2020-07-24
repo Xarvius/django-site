f3dc293b7a7c109d02bbd3be8985de436b805b3d
Arkdiusz Strzelec <strzelecki20@gmail.com>
2020-07-24 12:04:16 +0200

    create simple register form and model

--- user_manage/models.py
+++ user_manage/models.py
@@ -2,9 +2,23 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
-class userProfile(models.Model):
+class UserProfile(models.Model):
+    STUDENT = "ST"
+    PROFESOR = "PROF"
+    STATUS_IN = [
+        (STUDENT, 'STUDENT'),
+        (PROFESOR, "PROFESOR")
+    ]
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    profile_pic = models.ImageField(upload_to='profile_pics', blank=True)
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    phone = models.IntegerField()
+    date_begin = models.DateField()
+    status = models.CharField(
+        max_length=4,
+        choices=STATUS_IN,
+        default=PROFESOR
+    )
 
     def __str__(self):
         return self.user.username
