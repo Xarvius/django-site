f39810cee6838e116a7f55f4e92ad59c5085db19
Arkdiusz Strzelec <strzelecki20@gmail.com>
2020-07-24 14:13:43 +0200

    home,login&register views

--- user_manage/forms.py
+++ user_manage/forms.py
@@ -4,11 +4,11 @@ from django.contrib.auth.models import User
 
 class UserForm(forms.ModelForm):
     password = forms.CharField(widget=forms.PasswordInput())
-    class Meta():
+    class Meta:
         model = User
         fields = ('username', 'password', 'email')
 
 class UserProfileForm(forms.ModelForm):
-    class Meta():
+    class Meta:
         model = UserProfile
         fields = ('first_name', 'last_name', 'phone', 'date_begin', 'status')
